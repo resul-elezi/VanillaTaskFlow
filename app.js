3bce322ee0db5e65cd9821a379547c0ecdb6bf5e
Resul Elezi <resul_elezi@protonmail.com>
2025-02-06 10:48:44 +0100

    costumized app.js and index.html

--- app.js
+++ app.js
@@ -4,3 +4,16 @@
 const taskForm = document.getElementById("task-form");
 const taskInput = document.getElementById("new-task-input");
 const allTasksContainer = document.getElementById("all-tasks");
+
+// Function for adding a task
+function addTask(taskContent) {
+    const li = document.createElement("li");
+    li.textContent = taskContent;
+}
+
+// Button for marking the task as completed
+const completeBtn = document.createElement("button");
+completeBtn.textContent = "Complete";
+completeBtn.addEventListener("click", () => {
+    markTaskAsCompleted(li);
+})
