35ac3ac0aa1511131682dc399da64040ede72c02
Resul Elezi <resul_elezi@protonmail.com>
2025-02-06 10:42:25 +0100

    updated app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// DOM-Elements
+const taskForm = document.getElementById("task-form");
+const taskInput = document.getElementById("new-task-input");
+const allTasksContainer = document.getElementById("all-tasks");
